058ef70bf3cd443867571af1eab51b5d4b79d5d1
Yasir Ali <66301747+YasirAli851@users.noreply.github.com>
2023-08-06 19:29:05 +0530

    Update Add_Binary.cpp

--- Mathematics/LEETCODE/EASY LEVEL/Add_Binary.cpp
+++ Mathematics/LEETCODE/EASY LEVEL/Add_Binary.cpp
@@ -12,32 +12,34 @@ Output: "10101"
 My Approach:-
 
 Step 1:-I will check whether the size of both the strings a and b is same or not.If any of the string will be shorter then i will find the difference 
-between the size of string a and string b and append zeroes (size of a - size of b if string a is larger ) times or (size of b - size of a if string b is
-larger ) times at the the beginning of shorter string to make both the string equal for further calculations.
+between the size of string a and string b and append zeroes (size of a - size of b if string a is larger ) times or (size of b - size of a if string
+b is larger ) times at the the beginning of shorter string to make both the string equal for further calculations.
 
 step 2:-in step 2,we are creating a vector called carry where we will keep the carry.
 
-step 3:-initially our vector named carry will be empty.now,we will start iterating from the last index of any string between two(string a or string b),
+step 3:-initially our vector named carry will be empty.now,we will start iterating from the last index of any string between two(string a or string b)
 and check for the following conditions:-
 
-    condition 1:-we will check if string a[i]==0 && string b[i]==1 or string a[i]==1 && string b[i]==0,if this  condition will be satisfied then we will 
-    check that whether our vector of carry is empty or not,if vector of carry is empty then we will simply put "1" in the resultant string.if vector of 
-    carry will not be empty then we will first empty the vector of carry then we will put "0" in the resultant string and then push_back 1 in the carry.
+    condition 1:-we will check if string a[i]==0 && string b[i]==1 or string a[i]==1 && string b[i]==0,if this  condition will be satisfied then we 
+    will check that whether our vector of carry is empty or not,if vector of carry is empty then we will simply put "1" in the resultant string.if 
+vector of carry will not be empty then we will first empty the vector of carry then we will put "0" in the resultant string and then push_back 1 in 
+the carry.
 
-    condition 2:-if condition 1 will not be satisfied then we will check this condition 2.So,we will check if string a[i]==1 && string b[i]==1,if this
-    condition will be satisfied then we  will check that whether our vector of carry is empty or not,if vector of carry is empty then we will simply  
-    put "0" in the resultant string and push_back 1 in the vector of carry.if vector of carry will not be empty then we will first empty the vector of 
-    carry then we will put "1" in the resultant string and then push_back 1 in the carry.
+    condition 2:-if condition 1 will not be satisfied then we will check this condition 2.So,we will check if string a[i]==1 && string b[i]==1,
+if this condition will be satisfied then we  will check that whether our vector of carry is empty or not,if vector of carry is empty then we will
+simply put "0" in the resultant string and push_back 1 in the vector of carry.if vector of carry will not be empty then we will first empty the 
+vector of carry then we will put "1" in the resultant string and then push_back 1 in the carry.
 
-    condition 3:-if condition 2 will not be satisfied then we will check this condition 3.So,we will check if string a[i]==0 && string b[i]==0,if this 
-    condition will be satisfied then we  will check that whether our vector of carry is empty or not,if vector of carry is empty then we will simply  put "0" 
-    in the resultant string.if vector of carry will not be empty then we will first empty the vector of  carry then we will put "1" in the resultant string.
+    condition 3:-if condition 2 will not be satisfied then we will check this condition 3.So,we will check if string a[i]==0 && string b[i]==0,
+if this condition will be satisfied then we  will check that whether our vector of carry is empty or not,if vector of carry is empty then we will 
+simply  put "0" in the resultant string.if vector of carry will not be empty then we will first empty the vector of  carry then we will put "1" in 
+the resultant string.
 
     above conditions will be checked for each and every index starting from (a.size()-1 or b.size()-1) to index 0.
 
 step 4:-after performing the step 3 we will check if the vector of carry is empty or not,if yes then we will simply reverse the resultant string and 
-return that resultant string.but if the vector of carry will not be empty then we will first add "1" at the end of the resultant string then reverse the 
-resultant string and then return it.
+return that resultant string.but if the vector of carry will not be empty then we will first add "1" at the end of the resultant string then reverse 
+the resultant string and then return it.
 
 My naive approached code:-
 
