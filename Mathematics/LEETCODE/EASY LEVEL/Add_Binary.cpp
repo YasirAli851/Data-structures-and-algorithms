1042c1badee15a6aeb264ea5dd4ebc8802aa32f1
Yasir Ali <66301747+YasirAli851@users.noreply.github.com>
2023-09-21 19:09:52 +0530

    Update Add_Binary.cpp

--- Mathematics/LEETCODE/EASY LEVEL/Add_Binary.cpp
+++ Mathematics/LEETCODE/EASY LEVEL/Add_Binary.cpp
@@ -1,6 +1,5 @@
 Link to the question:- https://leetcode.com/problems/add-binary/
 Given two binary strings a and b, return their sum as a binary string.
-
 Example 1:-
 Input: a = "11", b = "1"
 Output: "100"
